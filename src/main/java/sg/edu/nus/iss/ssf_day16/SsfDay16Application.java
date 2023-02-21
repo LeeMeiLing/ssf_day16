1356cdbe982e44f6ec6bfdbde0b007e2488cf7c3
LeeMeiLing <leem0060@outlook.com>
2023-02-21 12:36:52 +0800

    Endpoint that consume and produce diff data format

--- src/main/java/sg/edu/nus/iss/ssf_day16/SsfDay16Application.java
+++ src/main/java/sg/edu/nus/iss/ssf_day16/SsfDay16Application.java
@@ -23,53 +23,53 @@ public class SsfDay16Application implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		// {"name" : "fred", "email":"fred@gmail.com"}
-		// JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+		// // {"name" : "fred", "email":"fred@gmail.com"}
+		// // JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
 
-		// jsonBuilder = jsonBuilder.add("name","fred").
-		// 	add("email","fred@gmail.com");
+		// // jsonBuilder = jsonBuilder.add("name","fred").
+		// // 	add("email","fred@gmail.com");
 
-		// JsonObject json = jsonBuilder.build();
+		// // JsonObject json = jsonBuilder.build();
 
 
-		JsonObject json = Json.createObjectBuilder()
-			.add("name","fred")
-				.add("email","fred@gmail.com")
-				.build();
+		// JsonObject json = Json.createObjectBuilder()
+		// 	.add("name","fred")
+		// 		.add("email","fred@gmail.com")
+		// 		.build();
 
-		System.out.printf("++++++++ json: %s\n",json.toString());
+		// System.out.printf("++++++++ json: %s\n",json.toString());
 
-		JsonArray array = Json.createArrayBuilder()
-			.add(json)
-			.add(json)
-			.build();
+		// JsonArray array = Json.createArrayBuilder()
+		// 	.add(json)
+		// 	.add(json)
+		// 	.build();
 
-		System.out.printf("++++++++ array: %s\n",array.toString());
+		// System.out.printf("++++++++ array: %s\n",array.toString());
 
-		String jsonStr = """
-			{	"orderId":1234, "address" : "10 Bedrock Ave",
-				"lineItems" : [
-					{"name" :"apple" , "quantity":10},
-					{"name" :"orange" , "quantity":20}
-				]
-			}
-		""";
+		// String jsonStr = """
+		// 	{	"orderId":1234, "address" : "10 Bedrock Ave",
+		// 		"lineItems" : [
+		// 			{"name" :"apple" , "quantity":10},
+		// 			{"name" :"orange" , "quantity":20}
+		// 		]
+		// 	}
+		// """;
 
-		System.out.printf("++++++++ jsonStr: \n%s\n", jsonStr);
+		// System.out.printf("++++++++ jsonStr: \n%s\n", jsonStr);
 
-		Reader reader = new StringReader(jsonStr); // can use inpurstream
-		JsonReader jsonReader= Json.createReader(reader); // Json.createReader() can accept input stream
-		json = jsonReader.readObject();
+		// Reader reader = new StringReader(jsonStr); // can use input stream
+		// JsonReader jsonReader= Json.createReader(reader); // Json.createReader() can accept input stream
+		// json = jsonReader.readObject();
 
-		System.out.println("\n ======================================================");
-		System.out.printf("orderId: %d\n",json.getInt("orderId"));
-		System.out.printf("address: %s\n",json.getString("address"));
-		array = json.getJsonArray("lineItems");
-		for(int i=0; i< array.size(); i++){
-			json = array.getJsonObject(i);
-			System.out.printf("\t name: %s, quantity: %d\n",json.getString("name"),
-				json.getInt("quantity"));
-		}
+		// System.out.println("\n ======================================================");
+		// System.out.printf("orderId: %d\n",json.getInt("orderId"));
+		// System.out.printf("address: %s\n",json.getString("address"));
+		// array = json.getJsonArray("lineItems");
+		// for(int i=0; i< array.size(); i++){
+		// 	json = array.getJsonObject(i);
+		// 	System.out.printf("\t name: %s, quantity: %d\n",json.getString("name"),
+		// 		json.getInt("quantity"));
+		// }
 
 	}
 
---
+++ src/main/java/sg/edu/nus/iss/ssf_day16/controller/RSVPController.java
@@ -0,0 +1,80 @@
+package sg.edu.nus.iss.ssf_day16.controller;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+
+@RestController
+@RequestMapping(path = "/rsvp" , produces = MediaType.APPLICATION_JSON_VALUE)
+public class RSVPController {
+
+    private Logger logger = Logger.getLogger(RSVPController.class.getName());
+
+// Request
+// > POST /rsvp
+// > Content-Type: application/x-www-form-urlencoded
+// > Accept: application/json
+@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+public ResponseEntity<String> postAsForm(@RequestBody MultiValueMap<String,String> form){
+
+    String name = form.getFirst("name");
+    boolean attending = Boolean.parseBoolean(form.getFirst("attending"));
+
+    logger.log(Level.INFO, "\n >>> name: %s, attending: %b".formatted(name,attending));
+
+
+    // 201 Created
+    // Content-Type: application/json
+    //
+    // { "status" : "Received <name> RSVP" } 
+
+    // String statusValue = "Received <" + name + "> RSVP";
+    // JsonObject payload = Json.createObjectBuilder().add("status",statusValue).build();
+
+    JsonObject payload = Json.createObjectBuilder().add("message","Received %s RSVP".formatted(name))
+        .add("attending", attending)
+        .build();
+    return ResponseEntity.status(201).body(payload.toString());
+}
+
+
+// POST request Content-Type: application/json
+@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity<String> postAsJson(@RequestBody String payload){
+
+    logger.log(Level.INFO, "\n +++ payload: %s".formatted(payload));
+
+    // read the payload and convert into json object
+    Reader reader = new StringReader(payload); 
+    JsonReader jsonReader= Json.createReader(reader); 
+    JsonObject json = jsonReader.readObject();
+
+    String name = json.getString("name");
+    boolean attending = json.getBoolean("attending");
+
+    logger.log(Level.INFO, "\n >>> JSON name: %s, attending: %b".formatted(name,attending));
+
+
+    // can reuse jsonobject 'json' here
+    JsonObject returnPayload = Json.createObjectBuilder().add("message","Received %s RSVP".formatted(name))
+        .add("attending", attending)
+        .build();
+
+    return ResponseEntity.status(201).body(returnPayload.toString());
+
+}
+    
+}
